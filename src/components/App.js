b433ae3cb0c245cebe7faaeff51881b23300c95e
Noel Kirkland <noelkirkland@gmail.com>
2020-10-15 13:02:58 -0600

    create a paragraph componenet and add a background image to the application

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import Paragraph from './Paragraph';
 import ZombieCount from './ZombieCount';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <Paragraph />
       <ZombieCount />
     </React.Fragment>
   );
---
+++ src/components/Paragraph.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Paragraph(){
+  return(
+    <p>This is a state of the are zombie tracker web app. It can be used for tracking wil zombie populations</p>
+  )
+}
+
+export default Paragraph;
